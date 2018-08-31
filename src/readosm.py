5782f8a466875390c771a546c04790f6fc2264a8
tokudaek <tokudaek@gmail.com>
2018-08-31 13:02:22 -0300

    Include nearest neighbour functionality

--- src/readosm.py
+++ src/readosm.py
@@ -3,22 +3,25 @@
 """
 
 import numpy as np
+import numpy
+import numpy.linalg
+from numpy.linalg import norm as norm
 import argparse
-#import osmium
-#import shapely.wkb as wkblib
 import xml.etree.ElementTree as ET
 from rtree import index
 import matplotlib.pyplot as plt
 import logging
 from logging import debug
 import random
+import time
 
-# Definitions
+########################################################## DEFS
 WAY_TYPES = ["motorway", "trunk", "primary", "secondary", "tertiary",
              "unclassified", "residential", "service", "living_street"]
+MAX_ARRAY_SZ = 1048576  # 2^20
 
 ##########################################################
-def get_all_nodes(root, invways):
+def parse_nodes(root, invways):
     """Get all nodes in the xml struct
 
     Args:
@@ -28,8 +31,8 @@ def get_all_nodes(root, invways):
     rtree.index: rtree of the nodes
     invways(dict): inverted list of ways, i.e., node as key and list of way ids as values
     """
+    t0 = time.time() 
     valid = invways.keys()
-    nodesidx = index.Index()
     nodeshash = {}
 
     for child in root:
@@ -38,14 +41,14 @@ def get_all_nodes(root, invways):
 
         att = child.attrib
         lat, lon = float(att['lat']), float(att['lon'])
-        nodesidx.insert(int(att['id']), (lat, lon, lat, lon))
         nodeshash[int(att['id'])] = (lat, lon)
-    debug('Found {} path nodes'.format(len(nodeshash.keys())))
+    debug('Found {} (traversable) nodes ({:.3f}s)'.format(len(nodeshash.keys()),
+                                                          time.time() - t0))
 
-    return nodesidx, nodeshash
+    return nodeshash
 
 ##########################################################
-def get_all_ways(root):
+def parse_ways(root):
     """Get all ways in the xml struct
 
     Args:
@@ -55,6 +58,8 @@ def get_all_ways(root):
     dict of list: hash of wayids as key and list of nodes as values;
     dict of list: hash of nodeid as key and list of wayids as values;
     """
+
+    t0 = time.time()
     ways = {}
     invways = {} # inverted list of ways
 
@@ -73,35 +78,21 @@ def get_all_ways(root):
                     isstreet = True
 
         if isstreet:
-            ways[wayid]  = nodes
+            ways[wayid] = nodes
 
             for node in nodes:
                 if node in invways.keys(): invways[node].append(wayid)
                 else: invways[node] = [wayid]
 
-    debug('Found {} ways'.format(len(ways.keys())))
+    debug('Found {} ways ({:.3f}s)'.format(len(ways.keys()), time.time() - t0))
     return ways, invways
 
 ##########################################################
-def idx2array_nodes(nodes_rtree):
-    bounds = nodes_rtree.bounds
-    nodeslist = list(nodes_rtree.intersection(bounds, objects=True))
-    npoints = len(nodeslist)
-
-    nodes = np.ndarray((npoints, 2))
-
-    for i, node in enumerate(nodeslist):
-        nodes[i, 0] = node.bbox[0]
-        nodes[i, 1] = node.bbox[1]
-
-    return nodes
-
-##########################################################
-def render_map(nodeshash, ways, crossings, frontend='bokeh'):
+def render_map(nodeshash, ways, crossings, artpoints, queries, frontend='bokeh'):
     if frontend == 'matplotlib':
-        render_matplotlib(nodeshash, ways, crossings)
+        render_matplotlib(nodeshash, ways, crossings, artpoints, queries)
     else:
-        render_bokeh(nodeshash, ways, crossings)
+        render_bokeh(nodeshash, ways, crossings, artoints, queries)
 
 ##########################################################
 def get_nodes_coords_from_hash(nodeshash):
@@ -123,7 +114,7 @@ def get_nodes_coords_from_hash(nodeshash):
 
 ##########################################################
 def get_crossings(invways):
-    """Get the crossings
+    """Get crossings
 
     Args:
     invways(dict of list): inverted list of the ways. It s a dict of nodeid as key and
@@ -154,6 +145,7 @@ def filter_out_orphan_nodes(ways, invways, nodeshash):
     Returns:
     dict of list, dict of lists
     """
+
     ninvways = len(invways.keys())
     nnodeshash = len(nodeshash.keys())
     if ninvways == nnodeshash: return ways, invways
@@ -176,19 +168,167 @@ def filter_out_orphan_nodes(ways, invways, nodeshash):
     debug('Filtered {} orphan nodes.'.format(ninvways - nnodeshash))
     return ways, invways
 
+##########################################################
+def get_segments(ways, crossings):
+    """Get segments, given the ways and the crossings
+
+    Args:
+    ways(dict of list): hash of wayid as key and list of nodes as values
+    crossings(set): set of nodeids in crossings
+
+    Returns:
+    dict of list: hash of segmentid as key and list of nodes as value
+    dict of list: hash of nodeid as key and list of sids as value
+    """
+
+    t0 = time.time() 
+    segments = {}
+    invsegments = {}
+
+    sid = 0 # segmentid
+    segment = []
+    for w, nodes in ways.items():
+        if not crossings.intersection(set(nodes)):
+            segments[sid] = nodes
+            sid += 1
+            continue
+
+        segment = []
+        for node in nodes:
+            segment.append(node)
+
+            if node in crossings and len(segment) > 1:
+                segments[sid] = segment
+                for snode in segment:
+                    if snode in invsegments: invsegments[snode].append(sid) 
+                    else: invsegments[snode] = [sid]
+                segment = [node]
+                sid += 1
+
+        segments[sid] = segment # Last segment
+        for snode in segment:
+            if snode in invsegments: invsegments[snode].append(sid) 
+            else: invsegments[snode] = [sid]
+        sid += 1
+
+    debug('Found {} segments ({:.3f}s)'.format(len(segments.keys()), time.time() - t0))
+    return segments, invsegments
 
 ##########################################################
-def render_matplotlib(nodeshash, ways, crossings):
+def evenly_space_segment(segment, nodeshash, epsilon):
+    """Evenly space one segment
+
+    Args:
+    segment(list): nodeids composing the segment
+    nodeshash(dict of list): hash with nodeid as value and 2-uple as value
+
+    Returns:
+    coords(ndarray(., 2)): array of coordinates
+    """
+    #debug(segment)
+    prevnode = np.array(nodeshash[segment[0]])
+    points = [prevnode]
+
+    for nodeid in segment[1:]:
+        node = np.array(nodeshash[nodeid])
+        d = norm(node - prevnode)
+        if d < epsilon:
+            points.append(node)
+            prevnode = node
+            continue
+
+        nnewnodes = int(d / epsilon)
+        direc = node - prevnode
+        direc = direc / norm(direc)
+
+        cur = prevnode
+        for i in range(nnewnodes):
+            newnode = cur + direc*epsilon
+            points.append(newnode)
+            cur = newnode
+        prevnode = node
+    return np.array(points)
+
+##########################################################
+def evenly_space_segments(segments, nodeshash, epsilon=0.0001):
+#def evenly_space_segments(segments, nodeshash, epsilon=1):
+    """Evenly space all segments and create artificial points
+
+    Args:
+    segments(dict of list): hash of segmentid as key and list of nodes as values
+    nodeshash(dict of list): hash with nodeid as value and 2-uple as value
+
+    Returns:
+    points(ndarray(., 3)): rows represents points and first and second columns
+    represent coordinates and third represents the segmentid
+    """
+
+    t0 = time.time()
+    points = np.ndarray((MAX_ARRAY_SZ, 3))
+    idx = 0
+    for sid, segment in segments.items():
+        coords = evenly_space_segment(segment, nodeshash, epsilon)
+        n, _ = coords.shape
+        points[idx:idx+n, 0:2] = coords
+        points[idx:idx+n, 2] = sid
+        idx = idx + n
+    debug('1-D grid points created ({:.3f}s)'.format(time.time() - t0))
+    return points[:idx, :]
+
+##########################################################
+def create_rtree(points, nodeshash, invsegments, invways):
+    """short-description
+
+    Args:
+    params
+
+    Returns:
+    ret
+
+    Raises:
+    """
+
+    t0 = time.time()
+    pointsidx = index.Index()
+
+    for pid, p in enumerate(points):
+        lat, lon  = p[0:2]
+        sid = int(p[2])
+        pointsidx.insert(pid, (lat, lon, lat, lon), sid)
+    
+    debug('R-tree created ({:.3f}s)'.format(time.time() - t0))
+
+    return pointsidx
+
+##########################################################
+def test_query(pointsidx, points):
+    t0 = time.time()
+    for i in range(10):
+        idx = np.random.randint(1000)
+        x0 = points[idx, 0]
+        y0 = points[idx, 0]
+        querycoords = (x0, y0, x0, y0)
+        list(pointsidx.nearest(querycoords, num_results=1, objects='raw'))
+    elapsed = time.time() - t0
+    debug(elapsed)
+##########################################################
+def render_matplotlib(nodeshash, ways, crossings, artpoints, queries):
     # render nodes
     nodes = get_nodes_coords_from_hash(nodeshash)
     #plt.scatter(nodes[:, 1], nodes[:, 0], c='blue', alpha=1, s=20)
 
+    # render artificial nodes
+    #plt.scatter(artpoints[:, 1], artpoints[:, 0], c='blue', alpha=1, s=20)
+
+
     # render ways
+    colors = {}
     i = 0
-    for wnodes in ways.values():
+    for wid, wnodes in ways.items():
         i += 1
         r = lambda: random.randint(0,255)
         waycolor = '#%02X%02X%02X' % (r(),r(),r())
+        colors[wid] = waycolor
         lats = []; lons = []
         for nodeid in wnodes:
             a, o = nodeshash[nodeid]
@@ -196,16 +336,20 @@ def render_matplotlib(nodeshash, ways, crossings):
             lons.append(o)
         plt.plot(lons, lats, linewidth=2, color=waycolor)
 
+    # render queries
+    for q in queries:
+        plt.scatter(q[1], q[0], linewidth=2, color=colors[q[2]])
+
     # render crossings
     crossingscoords = np.ndarray((len(crossings), 2))
     for j, crossing in enumerate(crossings):
         crossingscoords[j, :] = np.array(nodeshash[crossing])
-    plt.scatter(crossingscoords[:, 1], crossingscoords[:, 0], c='black')
+    #plt.scatter(crossingscoords[:, 1], crossingscoords[:, 0], c='black')
     #plt.axis('equal')
     plt.show()
 
 ##########################################################
-def render_bokeh(nodeshash, ways, crossings):
+def render_bokeh(nodeshash, ways, crossings, artpoints):
     nodes = get_nodes_coords_from_hash(nodeshash)
 
     from bokeh.plotting import figure, show, output_file
@@ -238,77 +382,50 @@ def render_bokeh(nodeshash, ways, crossings):
     show(p)  # open a browser
     return
 
-##########################################################
-def get_segments(ways, crossings):
-    """Get segments, given the ways and the crossings
-
-    Args:
-    ways(dict of list): hash of wayid as key and list of nodes as values
-    crossings(set): set of nodeids in crossings
-
-    Returns:
-    dict of list: hash of segmentid as key and list of nodes as value
-    dict of list: hash of nodeid as key and list of sids as value
-    """
-
-    segments = {}
-    invsegments = {}
-
-    sid = 0 # segmentid
-    segment = []
-    for w, nodes in ways.items():
-        if not crossings.intersection(set(nodes)):
-            segments[sid] = nodes
-            sid += 1
-            continue
-
-        segment = []
-        for node in nodes:
-            segment.append(node)
-
-            if node in crossings and len(segment) > 1:
-                segments[sid] = segment
-                for snode in segment:
-                    if snode in invsegments: invsegments[snode].append(sid) 
-                    else: invsegments[snode] = [sid]
-                segment = [node]
-                sid += 1
-
-        segments[sid] = segment # Last segment
-        for snode in segment:
-            if snode in invsegments: invsegments[snode].append(sid) 
-            else: invsegments[snode] = [sid]
-        sid += 1
-
-    debug('Found {} segments'.format(len(segments.keys())))
-    return segments, invsegments
 
 ##########################################################
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('inputosm', help='Input osm file')
     parser.add_argument('--frontend', choices=['bokeh', 'matplotlib'],
-                        help='Front end vis')
+                        help='Front-end vis')
     parser.add_argument('--verbose', help='verbose', action='store_true')
 
     args = parser.parse_args()
 
-    args.verbose = True # TODO: remove it
-
-    if args.verbose:
+    #if args.verbose:
+    if True:
         loglevel = args.verbose if logging.DEBUG else logging.ERROR
 
     logging.basicConfig(level=loglevel)
 
+
     tree = ET.parse(args.inputosm)
     root = tree.getroot() # Tag osm
 
-    ways, invways = get_all_ways(root)
-    nodestree, nodeshash = get_all_nodes(root, invways)
-    #ways, invways = filter_out_orphan_nodes(ways, invways, nodeshash)
+    ways, invways = parse_ways(root)
+    nodeshash = parse_nodes(root, invways)
+    ways, invways = filter_out_orphan_nodes(ways, invways, nodeshash)
     crossings = get_crossings(invways)
     segments, invsegments = get_segments(ways, crossings)
-    render_map(nodeshash, segments, crossings, args.frontend)
+    artpoints = evenly_space_segments(segments, nodeshash)
+    artpointstree = create_rtree(artpoints, nodeshash, invsegments, invways)
+
+    queried = []
+    for i in range(1000):
+        nartpoints, _ = artpoints.shape
+        idx = np.random.randint(nartpoints)
+        x = np.random.rand()/1000
+        y = np.random.rand()/1000
+        x0 = artpoints[idx, 0] + x
+        y0 = artpoints[idx, 1] + y
+        querycoords = (x0, y0, x0, y0)
+        segid = list(artpointstree.nearest(querycoords, num_results=1, objects='raw'))[0]
+        queried.append([x0, y0, segid])
+
+    queried = np.array(queried)
+
+    render_map(nodeshash, segments, crossings, artpoints, queried, args.frontend)
     
 ##########################################################
 if __name__ == '__main__':
